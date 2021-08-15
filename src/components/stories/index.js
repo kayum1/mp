1f3267a7d717131dd272a6f5d4275611265a235d
Kayum Amzad <kayum2@gmail.com>
2021-08-15 11:34:18 -0400

    update

--- src/components/stories/index.js
+++ src/components/stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Story from '../story';
 
 import { Wrapper, PageHeader } from './styles';
+const test = 'Test';
 const Stories = (props) => {
   return (
     <Wrapper>
